8a6513346474b804f8a58207a481a0b5edf0560e
helensereda <sereda.elenaby@gmail.com>
2023-07-15 22:29:08 +0300

    new3

--- src/test/java/CssSelectorsTest.java
+++ src/test/java/CssSelectorsTest.java
@@ -10,12 +10,13 @@ import utils.configuration.ReadProperties;
 public class CssSelectorsTest {
     protected WebDriver driver;
 
+
+
     @BeforeMethod
     public void setUp() {
         BrowserFactory browserFactory = new BrowserFactory();
         driver = browserFactory.getDriver();
     }
-
 
 
     @AfterMethod
