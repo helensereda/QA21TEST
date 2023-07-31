6b66a06c25bb00921afb4886d56238d78de4bf34
helensereda <sereda.elenaby@gmail.com>
2023-07-31 19:49:19 +0300

    commit

---
+++ src/test/java/tests/ScreenShotTest_homework.java
@@ -0,0 +1,28 @@
+package tests;
+
+import baseEntities.BaseTest;
+import io.qameta.allure.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ScreenShotTest_homework extends BaseTest {
+
+    @Test
+    @Story("AQA21-1100")
+    @TmsLink("TestCase-17")
+    @Description("Test without waitService")
+    @Severity(SeverityLevel.NORMAL)
+    public void infoAlertTest() {
+        driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+
+        driver.findElement(By.xpath("//button[. = 'Click for JS Alert']")).click();
+
+        Alert alert = driver.switchTo().alert();
+
+        Assert.assertEquals(alert.getText(), "I am a JS Alert");
+
+        alert.accept();
+    }
+}
