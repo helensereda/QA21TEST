53129ccf502236e9f11fb7461eb51f4d10d51ef4
helensereda <sereda.elenaby@gmail.com>
2023-07-31 19:52:15 +0300

    commit

--- src/test/java/tests/ScreenShotTest_homework.java
+++ src/test/java/tests/ScreenShotTest_homework.java
@@ -12,17 +12,13 @@ public class ScreenShotTest_homework extends BaseTest {
     @Test
     @Story("AQA21-1100")
     @TmsLink("TestCase-17")
-    @Description("Test without waitService")
+    @Description("Test with alert")
     @Severity(SeverityLevel.NORMAL)
     public void infoAlertTest() {
         driver.get("http://the-internet.herokuapp.com/javascript_alerts");
-
         driver.findElement(By.xpath("//button[. = 'Click for JS Alert']")).click();
-
         Alert alert = driver.switchTo().alert();
-
         Assert.assertEquals(alert.getText(), "I am a JS Alert");
-
         alert.accept();
     }
 }
