1ea5ad7df3a23d385534d6bc380f11799727b7fb
helensereda <sereda.elenaby@gmail.com>
2023-08-03 11:26:30 +0300

    commit EndToEndTest_hw

--- src/test/java/tests/EndToEndTest_hw.java
+++ src/test/java/tests/EndToEndTest_hw.java
@@ -9,23 +9,12 @@ import utils.configuration.ReadProperties;
 public class EndToEndTest_hw extends BaseTest_hw {
 
     @Test
-    public void successLoginTest() throws InterruptedException {
-
-        Assert.assertTrue(
-                loginStep.successLogin(ReadProperties.username(), ReadProperties.password()
-                ).isPageOpened());
-
-        Assert.assertTrue(
-                addCartStep.successAddCart().isPageOpened());
-
-        Assert.assertTrue(
-                checkCartStep.successCheckCart().isPageOpened());
-
-        Assert.assertTrue(
-                addInfoStep.successInfo(ReadProperties.firstname(), ReadProperties.lastname(), ReadProperties.code()).isPageOpened());
-
-        Assert.assertTrue(
-                finishStep.successFinish().isPageOpened());
+    public void successTest()  {
+                loginStep.successLogin(ReadProperties.username(), ReadProperties.password()).isPageOpened();
+                addCartStep.successAddCart().isPageOpened();
+                checkCartStep.successCheckCart().isPageOpened();
+                addInfoStep.successInfo(ReadProperties.firstname(), ReadProperties.lastname(), ReadProperties.code()).isPageOpened();
+                Assert.assertTrue(finishStep.successFinish().isPageOpened());
     }
 }
 
