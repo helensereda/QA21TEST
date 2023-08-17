4377a62ed6eb2a9a1f6e1299b9cb3812075ff323
helensereda <sereda.elenaby@gmail.com>
2023-08-17 23:35:13 +0300

    commit selenide

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -1,24 +1,25 @@
 package steps;
 
 import baseEntities.BaseStep;
+import models.User;
 import org.openqa.selenium.WebDriver;
 import pages.DashboardPage;
 import pages.LoginPage;
 
 public class LoginStep extends BaseStep {
 
-    public LoginStep(WebDriver driver) {
-        super(driver);
+    public LoginStep() {
+        super();
     }
 
-    public DashboardPage successLogin(String username, String psw) {
-        loginPage.login(username, psw);
+    public DashboardPage successLogin(User user) {
+        loginPage.login(user);
 
         return dashboardPage;
     }
 
-    public LoginPage negativeLogin(String username, String psw) {
-        loginPage.login(username, psw);
+    public LoginPage negativeLogin(User user) {
+        loginPage.login(user);
 
         return loginPage;
     }
