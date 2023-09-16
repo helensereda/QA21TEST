92c49b3c1b60a36debf02ad1d78a43a10c7afce4
helensereda <sereda.elenaby@gmail.com>
2023-09-16 12:27:31 +0300

    commit builder

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -2,7 +2,6 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class Listener implements ITestListener {
-
     public void onTestFailure(ITestResult result) {
         System.out.println("Make a screenshot!!!");
     }
